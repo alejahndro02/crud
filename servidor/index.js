c4aa3f5e89c13e35fdd25f2136db08ac4255cad9
Alejahndro <alejahndro02@gmail.com>
2021-07-27 09:48:15 -0500

    Se agrego el middleware mediante app.use(express.json()), donde se aceptan archivos tipo json

--- servidor/index.js
+++ servidor/index.js
@@ -8,7 +8,8 @@ const app = express();
 //Se conecta a la Base de Datos
 conectarDB()
 
-//
+//se configura un middleware
+app.use(express.json())
 app.use('/api/productos', require('./routes/producto'));
 
 app.listen(4000, () => {})
