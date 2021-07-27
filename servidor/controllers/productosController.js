4ada4653a3fa1faa34ab5642873656bf7cf9e987
Alejahndro <alejahndro02@gmail.com>
2021-07-27 11:36:23 -0500

    Por medio del try..catch se crea el producto en la base de datos

--- servidor/controllers/productosController.js
+++ servidor/controllers/productosController.js
@@ -1,3 +1,17 @@
-exports.crearProducto = (req, res) => {
-    console.log(req.body);
+const Producto = require("../models/Producto");
+
+// const Producto = require('../models/Producto');
+exports.crearProducto = async(req, res) => {
+    try {
+        let producto;
+        //se crea el producto
+        producto = new Producto(req.body)
+
+        await producto.save();
+        res.send(producto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error')
+    }
 }
