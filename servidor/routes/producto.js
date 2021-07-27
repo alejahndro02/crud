c3d547e19cadb21d1c50c5e819d97f8a326ec39f
Alejahndro <alejahndro02@gmail.com>
2021-07-27 08:48:59 -0500

    Se agregaron las routas con router y express

---
+++ servidor/routes/producto.js
@@ -0,0 +1,9 @@
+// Rutas para el producto
+const express = require('express');
+const router = express.Router();
+
+//api/productos
+router.post('/', () => {
+    console.log('Creando producto...');
+})
+module.exports = router;
