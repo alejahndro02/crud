67ce2786711e32005ad57f3b1964c164c605da22
Alejahndro <alejahndro02@gmail.com>
2021-07-27 13:27:53 -0500

    Se ceo el metodo actualizar Productos, donde mediante el id se actualiza los datos en la base de datos

--- servidor/controllers/productosController.js
+++ servidor/controllers/productosController.js
@@ -5,14 +5,14 @@ exports.crearProducto = async(req, res) => {
     try {
         let producto;
         //se crea el producto
-        producto = new Producto(req.body)
+        producto = new Producto(req.body);
 
         await producto.save();
         res.send(producto);
 
     } catch (error) {
         console.log(error);
-        res.status(500).send('Hubo un error')
+        res.status(500).send('Hubo un error');
     }
 }
 
@@ -20,9 +20,28 @@ exports.obtenerProductos = async(req, res) => {
 
     try {
         const productos = await Producto.find();
-        res.json(productos)
+        res.json(productos);
     } catch (error) {
         console.log(error);
-        res.status(500).send('Hubo un error')
+        res.status(500).send('Hubo un error');
+    }
+}
+exports.actualizarProductos = async(req, res) => {
+    try {
+        const { nombre, categoria, ubicacion, precio } = req.body;
+        let producto = await Producto.findById(req.params.id);
+        if (!producto) {
+            res.status(404).json({ msg: 'No existe e producto' });
+        }
+        producto.nombre = nombre;
+        producto.categoria = categoria;
+        producto.ubicacion = ubicacion;
+        producto.precio = precio;
+        producto = await Producto.findOneAndUpdate({ _id: req.params.id }, producto, { new: true })
+        res.json(producto);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+
     }
 }
