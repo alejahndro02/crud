c28d774f25717d2d8b0bc77c403979809d8ba521
Alejahndro <alejahndro02@gmail.com>
2021-07-27 13:30:14 -0500

    Se crea el metodo put y se le pasa como parametro el id y el metodo creado en el controlador llamado actualizarProducto

--- servidor/routes/producto.js
+++ servidor/routes/producto.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const productoController = require('../controllers/productosController');
 
 //api/productos
-router.post('/', productoController.crearProducto)
-router.get('/', productoController.obtenerProductos)
+router.post('/', productoController.crearProducto);
+router.get('/', productoController.obtenerProductos);
+router.put('/:id', productoController.actualizarProductos);
+
 module.exports = router;
