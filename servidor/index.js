8ee8351c288886b74cf1d6431ab5a298408aeb6b
Alejahndro <alejahndro02@gmail.com>
2021-07-27 13:28:26 -0500

    Se agregaron signos de puntuacion

--- servidor/index.js
+++ servidor/index.js
@@ -6,10 +6,10 @@ const conectarDB = require('./config/db');
 const app = express();
 
 //Se conecta a la Base de Datos
-conectarDB()
+conectarDB();
 
 //se configura un middleware
-app.use(express.json())
+app.use(express.json());
 app.use('/api/productos', require('./routes/producto'));
 
-app.listen(4000, () => {})
+app.listen(4000, () => {});
